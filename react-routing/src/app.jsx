106a34925012a7cdd6b0224ca6807cda86b888e7
Monica Keosombath <mkeosom@uw.edu>
2016-11-08 12:52:43 -0800

    movie database

---
+++ react-routing/src/app.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+//import our CSS file
+//Webpack will actually merge the contents
+//of this file into an inline <style></style>
+//attribute within the <head> section, so that
+//the browser doesn't have to make another
+//network request to get the styles!
+//note that here we start the path with a `.`
+//to signal that this is a relative file
+//path and not a module in our node_modules
+//directory 
+import "./css/main.css";
+import {Link, IndexLink} from "react-router";
+//import syntax used in two forms, default exported identifer or import several identifiers you can do { }
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <IndexLink to="/" 
+                            activeClassName ="active">Products</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/cart"
+                            activeClassName="active">Cart</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        );
+    }
+}
---
+++ react-routing/src/products.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+//polyfill for the fetch() API so that we can use
+//it in Safari and older browsers
+//this module was already included in our package.json
+//so after you execute `npm install` this module will
+//be in the node_modules directory, so we can load it
+//simply by importing it's module name
+import "whatwg-fetch";
+import Movie from "./movie.jsx";
+
+const APIKEY = "f7cd0968648d7e6edd7352c72b477266";
+const BASE_URL = "https://api.themoviedb.org/3"
+const DISCOVER_API = BASE_URL + "/discover/movie?api_key=" + APIKEY;
+const GENRES_API = BASE_URL + "/genre/movie/list?api_key=" + APIKEY;
+
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        //initialize the component state to an empty object
+        this.state = {}
+    }
+
+    componentDidMount(){
+        fetch(DISCOVER_API)
+            .then(response => response.json())
+            .then(data => this.setState({movies: data}));
+    }
+
+    render() {
+        var totalPages;
+        var movies;
+
+        if(this.state.movies) {
+            totalPages = (<p>{this.state.movies.total_pages}
+             pages</p>);
+
+            //movies = [];
+            //this.state.movies.results.forEach(function(m){
+            //    movies.push(<Movie key={m.id} movie={m}/>);
+            //});
+
+            //map creats a new array and puts whatever you return in the target array ^ same as above
+            //short hand format
+            movies = this.state.movies.results.map(m=> <Movie key={m.id} movie={m} />)
+
+
+            
+        }
+        return (
+            <div className="container">
+                <h1>Products View</h1>
+                <p>some nifty products for sale</p>
+                {totalPages}
+                {movies}
+            </div>
+        );
+    }
+}
